1eaf4adf52ba8bbbd4197001ee824ab517e12efe
adminliux <1787624543@qq.com>
2015-02-10 17:37:09 +0800

    是

---
+++ src/main/java/applicationParent/applicationParent/App.java
@@ -0,0 +1,13 @@
+package applicationParent.applicationParent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
